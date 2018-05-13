184aefebae478ace331b0af5561cdf561f02ec58
孙昊翔 <673686754@qq.com>
2018-05-13 16:36:53 +0800

    no message

--- Hello/src/Hello.java
+++ Hello/src/Hello.java
@@ -1,10 +1,5 @@
 public class Hello {
   public static void main(String[] args) {
-    System.out.print('H');
-    System.out.print('e');
-    System.out.print('l');
-    System.out.print('l');
-    System.out.print('o');
-    System.out.print('!');
+    System.out.println("Hello!");
   }
 }
