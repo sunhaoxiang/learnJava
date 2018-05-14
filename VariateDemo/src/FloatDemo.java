64b0b73fb6bd9290d050106e8621ae133f5b36d3
孙昊翔 <673686754@qq.com>
2018-05-14 11:24:03 +0800

    no message

--- VariateDemo/src/FloatDemo.java
+++
@@ -1,8 +0,0 @@
-public class FloatDemo {
-  public static void main(String[] args) {
-    float f = 1234.328f;
-    System.out.println("f=" + f);
-    double d = 5623.465;
-    System.out.println("d=" + d);
-  }
-}
