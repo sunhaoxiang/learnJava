0b40c1381dd604ec02d3073715a897d22821515f
孙昊翔 <673686754@qq.com>
2018-05-23 08:52:14 +0800

    no message

--- src/com/shx/Student.java
+++ src/com/shx/Student.java
@@ -8,16 +8,12 @@ class Student extends Person {
     super(name);
   }
 
-  private int getScore() {
-    return this.score;
-  }
-
   void setScore(int score) {
     this.score = score;
   }
 
   void printScore() {
-    System.out.println(this.getName() + "'s score is " + this.getScore());
+    System.out.println(this.getName() + "'s score is " + this.score);
   }
 
   @Override
