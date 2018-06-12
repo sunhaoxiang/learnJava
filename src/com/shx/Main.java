1b61abb0fd68e0428e5e72865524c6b26ab52637
孙昊翔 <673686754@qq.com>
2018-06-12 14:25:39 +0800

    no message

--- src/com/shx/Main.java
+++ src/com/shx/Main.java
@@ -2,14 +2,18 @@ package com.shx;
 
 public class Main {
 
-  public static void main(String[] args) {
-    Student s = new Student("Xiao Hong");
-    s.setName("Xiao Hong");
-    s.setScore(98);
-    s.run();
-    s.printScore();
-    System.out.println(s.hello());
-    Person.getNumber(3);
-  }
+    enum Size { SMALL, MEDIUM, LARGE};
+
+    public static void main(String[] args) {
+        Student s = new Student("Xiao Hong");
+        s.setName("Xiao Hong");
+        s.setScore(98);
+        s.run();
+        s.printScore();
+        System.out.println(s.hello());
+        Person.getNumber(3);
+        Size small =Size.SMALL;
+        System.out.println(small);
+    }
 
 }
