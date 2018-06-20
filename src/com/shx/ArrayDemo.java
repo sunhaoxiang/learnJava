75b36212bade5a599209e4f6ad894b918dc718bb
孙昊翔 <673686754@qq.com>
2018-06-20 12:46:09 +0800

    no message

---
+++ src/com/shx/ArrayDemo.java
@@ -0,0 +1,10 @@
+package com.shx;
+
+public class ArrayDemo {
+    int data[] = new int[] {1, 2, 3};
+    public void log() {
+        for (int i = 0; i < data.length; i++) {
+            System.out.println(data[i]);
+        }
+    }
+}
--- src/com/shx/Main.java
+++ src/com/shx/Main.java
@@ -3,15 +3,17 @@ package com.shx;
 public class Main {
 
     public static void main(String[] args) {
-        int maxValue = Integer.MAX_VALUE; // 最大值
-        int minValue = Integer.MIN_VALUE; // 最小值
-        long result = (long)maxValue + 1;
-        System.out.println("Hello World!");
-        System.out.println(maxValue);
-        System.out.println(minValue);
-        System.out.println(maxValue + 1); // 数据溢出
-        System.out.println(minValue - 1); // 数据溢出
-        System.out.println(result); // 不溢出
+//        int maxValue = Integer.MAX_VALUE; // 最大值
+//        int minValue = Integer.MIN_VALUE; // 最小值
+//        long result = (long)maxValue + 1;
+//        System.out.println("Hello World!");
+//        System.out.println(maxValue);
+//        System.out.println(minValue);
+//        System.out.println(maxValue + 1); // 数据溢出
+//        System.out.println(minValue - 1); // 数据溢出
+//        System.out.println(result); // 不溢出
+        ArrayDemo demo = new ArrayDemo();
+        demo.log();
     }
 
 }
