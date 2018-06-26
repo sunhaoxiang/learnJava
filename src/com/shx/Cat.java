635d94a4be41b5c375d594b9c254fe69ab08f733
孙昊翔 <673686754@qq.com>
2018-06-26 21:15:05 +0800

    no message

--- src/com/shx/Cat.java
+++ src/com/shx/Cat.java
@@ -9,6 +9,15 @@ public class Cat {
     double weight;
     String species;
 
+    public Cat() {}
+
+    public Cat(String name, int month, double weight, String species) {
+        this.name = name;
+        this.month = month;
+        this.weight = weight;
+        this.species = species;
+    }
+
     public void run() {
         System.out.println("Cat run.");
     }
