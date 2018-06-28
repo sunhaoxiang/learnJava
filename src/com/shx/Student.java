52dc1ed426cb6f0d2367bd92aaa968db24afd19a
孙昊翔 <673686754@qq.com>
2018-06-28 14:00:34 +0800

    no message

--- src/com/shx/Student.java
+++ src/com/shx/Student.java
@@ -5,15 +5,17 @@ public class Student {
     private String studentName;
     private String studentSex;
     private int studentAge;
+    private Subject studentSubject;
 
     public Student() {
     }
 
-    public Student(String studentNo, String studentName, String studentSex, int studentAge) {
+    public Student(String studentNo, String studentName, String studentSex, int studentAge ,Subject studentSubject) {
         this.setStudentNo(studentNo);
         this.setStudentName(studentName);
         this.setStudentSex(studentSex);
         this.setStudentAge(studentAge);
+        this.setStudentSubject(studentSubject);
     }
 
     public String getStudentNo() {
@@ -45,11 +47,26 @@ public class Student {
     }
 
     public void setStudentAge(int studentAge) {
-        this.studentAge = studentAge;
+        if (studentAge < 10 || studentAge > 100) {
+            this.studentAge = 18;
+        } else {
+            this.studentAge = studentAge;
+        }
+    }
+
+    public Subject getStudentSubject() {
+        if (this.studentSubject == null) {
+            this.studentSubject = new Subject();
+        }
+        return studentSubject;
+    }
+
+    public void setStudentSubject(Subject studentSubject) {
+        this.studentSubject = studentSubject;
     }
 
     public String introduction() {
-        String str = "学生信息如下： \n姓名：" + this.getStudentName() + "\n学号：" + this.getStudentNo() + "\n性别：" + this.getStudentSex() + "\n年龄：" + this.getStudentAge();
+        String str = "学生信息如下： \n姓名：" + this.getStudentName() + "\n学号：" + this.getStudentNo() + "\n性别：" + this.getStudentSex() + "\n年龄：" + this.getStudentAge() + "\n所报专业名称：" + this.getStudentSubject().getSubjectName() + "\n学制年限：" + this.getStudentSubject().getSubjectLife();
         return str;
     }
 }
--- src/com/shx/Subject.java
+++ src/com/shx/Subject.java
@@ -50,4 +50,5 @@ public class Subject {
 
         return str;
     }
+
 }
